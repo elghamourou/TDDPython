8ec28ac9220414f76c648690a19e53f90488ce8f
Salaheddine EL GHAMOUROU <elghamourou@gmail.com>
2018-11-06 13:07:54 +0000

    comiting before refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -40,18 +40,30 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text=='1: Buy peacock feathers' for row in rows),
-            "New To-Do item didnt't apears in table"
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows],
+            f"1: Buy peacock feathers didnt't apears in table. Ccontent were:\n{table.text}"
         )
 
         # there is still a text box inviting her to enter
         # another item
-        self.fail('Finish the test')
+
+
         # she enters Use peacock feathers to make a fly
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
 
         # the page updates again and the page shows both items now
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows],
+                      f"1: Buy peacock feathers didnt't apears in table. Ccontent were:\n{table.text}"
+                      )
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows],
+                      f"2: Use peacock feathers to make a fly didnt't apears in table. Ccontent were:\n{table.text}")
+        self.fail('Finish the test')
         # than she sees that the site generated a unique url for her
 
         # she visits the url and at her joy, she finds out that
--- lists/tests.py
+++ lists/tests.py
@@ -18,3 +18,6 @@ class HomePageTest(TestCase):
         #self.assertIn('<title>To-Do lists</title>', html)
         #self.assertTrue(html.endswith('</html>'))
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
