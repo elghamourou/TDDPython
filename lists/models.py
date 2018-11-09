a564b7b1807a5887c9b5475c06f500469f3670a5
Salaheddine EL GHAMOUROU <elghamourou@gmail.com>
2018-11-09 12:20:35 +0000

    Adjust the model so that each item is associated with different list

--- lists/models.py
+++ lists/models.py
@@ -2,5 +2,37 @@ from django.db import models
 
 # Create your models here.
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.SET_DEFAULT)
+
+
+# CASCADE: when you delete the UserProfile, all related Photos will be deleted too.
+# This is the default. (So in answer to that aspect of your question, yes,
+#  if you delete your user account the photos will be deleted automatically.)
+#
+# PROTECT: this will stop you from deleting a UserProfile with related Photos,
+# raising a django.db.models.ProtectedError if you try. The idea would be that
+# the user would need to disassociate or delete all Photos before they could delete
+# their profile.
+#
+# SET_NULL: when you delete the UserProfile, all associated Photos will still exist
+# but will no longer be associated with any UserProfile.
+# This would require null=True in the ForeignKey definition.
+#
+# SET_DEFAULT: when you delete the UserProfile, all associated Photos will be changed
+# to point to their default UserProfile as specified by the default attribute in the ForeignKey
+# definition (you could use this to pass "orphaned" photos off to a certain
+# user - but this isn't going to be common, SET_NULL or SET() will be much more common)
+#
+# SET(): when you delete the UserProfile, the target of the Photos
+# ' ForeignKey will be set to the value passed in to the SET function,' \
+# ' or what it returns if it is a callable. (Sorry, I haven't explained that well,
+# but the docs have an example which explains better.)
+#
+# DO_NOTHING: when you delete the UserProfile, all related Photos will remain unaltered,
+# thus having a broken reference, unless you have used some other SQL to take care of it.
--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 
-from lists.models import Item
+from lists.models import Item, List
 # Create your tests here.
 class HomePageTest(TestCase):
     #def test_root_url_resolve_to_home_page(self):
@@ -50,14 +50,26 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), initial_count)
 
+
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+
+        list_ = List()
+        list_.save()
+
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -66,7 +78,9 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, second_item.text)
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -76,8 +90,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_display_all_list_items(self):
-        Item.objects.create(text = 'itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text = 'itemey 1', list = list_)
+        Item.objects.create(text='itemey 2', list = list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
