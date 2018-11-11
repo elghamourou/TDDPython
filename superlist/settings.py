ee3240d63229f7fa3e032255e4c4582f03e2d289
Salaheddine EL GHAMOUROU <elghamourou@gmail.com>
2018-11-11 15:14:41 +0000

    changed ALLOWED_HOST to allow virtual box server

--- superlist/settings.py
+++ superlist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xs3#jhn_i+3_3yi+mb0bn1wa&-g)a9o0z+=9^#uwb+h7sbk5h_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.102','localhost', '127.0.0.1']
 
 
 # Application definition
