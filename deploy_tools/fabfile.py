64d46f0b930acee1a85fdbe87f4708cdf5176cf2
Salaheddine EL GHAMOUROU <elghamourou@gmail.com>
2018-11-20 19:01:33 +0000

    correcting the fabric script

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,7 +5,7 @@ import random
 REPO_URL = 'https://github.com/elghamourou/TDDPython'
 def _create_directory_structure_if_necessary(site_folder):
      for subfolder in('database', 'static', 'virtualenv', 'source'):
-         run(f'mkdir -p {site_folder}/{subfolder}')
+         run(f'rm -fr {site_folder}/{subfolder} &&  mkdir -p {site_folder}/{subfolder}')
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.get'):
@@ -16,10 +16,11 @@ def _get_latest_source(source_folder):
     run(f'cd {source_folder} && git reset --hard {current_commit}')
 
 def _update_settings(source_folder, host):
-    setting_path = source_folder + '/superlists/settings.py'
+    site_name = '192.168.1.25'
+    setting_path = source_folder + '/superlist/settings.py'
     sed(setting_path, "DEBUG = True", "DEBUG = False")
-    sed(setting_path, "ALLOWED_HOSTS = .+$", f"ALLOWED_HOSTS = ['{site_name}']")
-    secret_key_file = source_folder + '/superlists/secret_key.py'
+    sed(setting_path, 'ALLOWED_HOSTS = .+$', f'ALLOWED_HOSTS = ["{site_name}"]')
+    secret_key_file = source_folder + '/superlist/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
@@ -38,6 +39,12 @@ def _update_static_files(source_folder):
 def _update_database(source_folder):
     run(f'cd {source_folder} && ../virtualenv/bin/python manage.py migrate --noinput')
 
+def _start_gunicorn(port, source_folder):
+    run(f'cd {source_folder} && ../virtualenv/bin/gunicorn -b 0.0.0.0:{port} superlist.wsgi:application')
+
+def _stop_gunicorn(port):
+    run(f'kill -9 $(lsof -t -i:{port})')
+
 
 def deploy():
     site_folder = f'/Users/{env.user}/sites/{env.host}'
@@ -48,3 +55,13 @@ def deploy():
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
+    _start_gunicorn(9999, source_folder)
+
+
+def start_gunicorn():
+    site_folder = f'/Users/{env.user}/sites/{env.host}'
+    source_folder = site_folder + '/source'
+    _start_gunicorn(9999, source_folder)
+
+def stop_gunicorn():
+    _stop_gunicorn(9999)
