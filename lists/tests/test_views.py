3e82589c191bdbba307d75cd18c61fd93d563b50
Salaheddine EL GHAMOUROU <elghamourou@gmail.com>
2018-11-25 13:14:45 +0000

    refactoring unit tests by separating model and view tests in separate files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
-from django.http import HttpRequest
-
 from lists.models import Item, List
 # Create your tests here.
+
+
 class HomePageTest(TestCase):
     #def test_root_url_resolve_to_home_page(self):
     #    found = resolve('/')
@@ -35,52 +33,10 @@ class HomePageTest(TestCase):
         #self.assertIn('A new list item', response.content.decode())
         #self.assertTemplateUsed(response, 'home.html')
 
-    # def test_display_all_list_items(self):
-    #     Item.objects.create(text = 'itemey 1')
-    #     Item.objects.create(text='itemey 2')
-    #
-    #     response = self.client.get('/')
-    #
-    #     self.assertIn('itemey 1', response.content.decode())
-    #     self.assertIn('itemey 2', response.content.decode())
-
-
     def test_only_saves_items_when_necessary(self):
         initial_count = Item.objects.count()
         self.client.get('/')
         self.assertEqual(Item.objects.count(), initial_count)
-
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-
-        list_ = List()
-        list_.save()
-
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(),2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, first_item.text)
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, second_item.text)
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -90,7 +46,7 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_display_only_items_for_taht_list(self):
+    def test_display_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text = 'itemey 1', list = correct_list)
         Item.objects.create(text='itemey 2', list = correct_list)
@@ -105,7 +61,8 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
-    def test_passes_corerct_list_to_template(self):
+
+    def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
